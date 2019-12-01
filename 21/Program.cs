4838df71c4a90a2b03d83b7aee5fcdb087cce38b
Sebastian Hew <jingfee@gmail.com>
2019-12-01 14:22:11 +0100

    template all days

---
+++ 21/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC._2019._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = GetInputFromFile();
+        }
+
+        static string[] GetInputFromFile()
+        {
+            var data = File.ReadAllLines("input.txt");
+            return data;
+        }
+    }
+}
