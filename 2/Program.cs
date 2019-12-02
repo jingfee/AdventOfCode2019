a7ac9db2b3e546bb152a5a2bf5e202901d05964b
Sebastian Hew <jingfee@gmail.com>
2019-12-02 12:00:36 +0100

    day 2

--- 2/Program.cs
+++ 2/Program.cs
@@ -8,13 +8,58 @@ namespace AoC._2019._2
     {
         static void Main(string[] args)
         {
-            var input = GetInputFromFile();
+            var breakOutput = 19690720;
+            for (var noun = 0; noun < 100; noun++)
+            {
+                for (var verb = 0; verb < 100; verb++)
+                {
+                    var input = GetInputFromFile();
+                    input[1] = noun;
+                    input[2] = verb;
+                    if (breakOutput == RunProgram(input))
+                    {
+                        Console.WriteLine(100 * noun + verb);
+                    }
+                }
+            }
         }
 
-        static string[] GetInputFromFile()
+        static int RunProgram(int[] input)
         {
-            var data = File.ReadAllLines("input.txt");
-            return data;
+            var pointer = 0;
+            int op;
+            while (true)
+            {
+                op = input[pointer];
+                if (op == 99)
+                {
+                    break;
+                }
+
+                input[input[pointer + 3]] = DoOperation(op, input[input[pointer + 1]], input[input[pointer + 2]]);
+                pointer += 4;
+            }
+
+            return input[0];
+        }
+
+        static int DoOperation(int operation, int input1, int input2)
+        {
+            switch (operation)
+            {
+                case 1:
+                    return input1 + input2;
+                case 2:
+                    return input1 * input2;
+                default:
+                    return -1;
+            }
+        }
+
+        static int[] GetInputFromFile()
+        {
+            var data = File.ReadAllText("input.txt");
+            return data.Split(',').Select(d => Int32.Parse(d)).ToArray();
         }
     }
 }
